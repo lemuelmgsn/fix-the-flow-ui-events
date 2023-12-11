8852024c04279b60012ba1a68419ab34da303bca
lemuelmgsn <magsinolemuel68@gmail.com>
2023-12-11 13:04:25 +0100

    eerste 3 buttons gemaakt
    click , dblclick en keyCode

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,29 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Button 1
+let frontend = document.querySelector('a:nth-of-type(1)');
+
+frontend.addEventListener("click", function(){
+  frontend.classList.toggle("colorswitch");
+});
+// Button 2
+let design = document.querySelector('a:nth-of-type(2)');
+
+design.addEventListener("dblclick", function(){
+  design.classList.toggle("colorswitch");
+});
+// Button 3
+let and = document.querySelector('a:nth-of-type(3)');
+
+document.addEventListener("keyup", function (Event) {
+  console.log(Event)
+  if (Event.keyCode == 76 ) {
+    and.classList.toggle("colorswitch")
+  }
+})
+// Button 4
+let 
+
+
