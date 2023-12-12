7361b710fe1d15af5f31e9d6dd39296271531255
lemuelmgsn <magsinolemuel68@gmail.com>
2023-12-12 15:26:31 +0100

    focus toegevoegd
    .

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,19 +7,21 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
-// Button 1
+// Button 1 --- click
 let frontend = document.querySelector('a:nth-of-type(1)');
 
 frontend.addEventListener("click", function(){
   frontend.classList.toggle("colorswitch");
 });
-// Button 2
+
+// Button 2 --- dblclick
 let design = document.querySelector('a:nth-of-type(2)');
 
 design.addEventListener("dblclick", function(){
   design.classList.toggle("colorswitch");
 });
-// Button 3
+
+// Button 3 --- KeyUp "L"
 let and = document.querySelector('a:nth-of-type(3)');
 
 document.addEventListener("keyup", function (Event) {
@@ -28,7 +30,31 @@ document.addEventListener("keyup", function (Event) {
     and.classList.toggle("colorswitch")
   }
 })
-// Button 4
-let 
+
+// Button 4 --- focus
+let development = document.querySelector('a:nth-of-type(4)');
+
+development.addEventListener("focusin", function(Event){
+
+  event.target.style.background = "var(--highlight-secondary)";
+  event.target.style.color = "var(--secondary)";
+
+},
+true,);
+
+development.addEventListener("focusout", function(Event){
+  
+  event.target.style.background = "var(--secondary)";
+  event.target.style.color = "var(--highlight-primary)";
+
+},
+true,);
+
+// Button 5 --- 'naam eventlistener'
+let sprint5 = document.querySelector('a:nth-of-type(5)');
+
+// sprint5.addEventListener("iets", function);
+
+
 
 
